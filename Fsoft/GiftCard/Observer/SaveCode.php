36acaf9c8bc037924223ec93a53423710c7f09ba
root <manutdvncom@gmail.com>
2021-03-30 18:05:18 +0700

    E

--- Fsoft/GiftCard/Observer/SaveCode.php
+++ Fsoft/GiftCard/Observer/SaveCode.php
@@ -2,60 +2,65 @@
 
 namespace Fsoft\GiftCard\Observer;
 
-use Magento\Framework\Event\Observer;
-use Fsoft\GiftCard\Model\HistoryFactory;
 use Fsoft\GiftCard\Model\GiftCardFactory;
+use Fsoft\GiftCard\Model\HistoryFactory;
 use Magento\Customer\Model\Session;
+use Magento\Framework\Event\Observer;
 use Magento\Framework\Math\Random;
+
 class SaveCode implements \Magento\Framework\Event\ObserverInterface
 {
     protected $historyFactory;
-    protected $giftcardFactory;
     protected $customerSession;
     protected $randomString;
-    public function __construct
-    (
+    protected $giftCardFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
         HistoryFactory $historyFactory,
-        GiftCardFactory $giftcardFactory,
         Session $customerSession,
         Random $randomString,
+        GiftCardFactory $giftCardFactory,
         array $data = []
-    )
-    {
+    ) {
         $this->historyFactory = $historyFactory;
-        $this->giftcardFactory = $giftcardFactory;
         $this->customerSession = $customerSession;
         $this->randomString = $randomString;
+        $this->giftCardFactory = $giftCardFactory;
     }
     public function execute(Observer $observer)
     {
-        $quote = $observer->getEvent()->getQuote();
         $order = $observer->getEvent()->getOrder();
+
+        /** @var \Magento\Quote\Model\Quote $quote */
+        $quote = $observer->getEvent()->getQuote();
         $customer_id = $this->customerSession->getCustomer()->getId();
+        $increment_id = $order->getIncrementId();
+        $order_id = !empty($order->getData('order_id')) ? $order->getData('order_id') : null;
         $history = $this->historyFactory->create();
-        $giftcard = $this->giftcardFactory->create();
-        $order_id = $order->getId();
-        $quote_id = $quote->getId();
-        $items = $order->getAllItems();
-        $quote_item = $quote->getAllItems();
-        foreach($items as $item){
-            if ($order_id){
+        $giftcard = $this->giftCardFactory->create();
+        $random = $this->randomString->getRandomString(12);
+        foreach ($order->getAllItems() as $item) {
+            if ($order_id) {
                 $history->load($order_id);
-                $newData = [
-                    'giftcard_id' => $item->getData('order_id'),
+                $history->addData([
+                    'giftcard_id' => $order_id,
                     'customer_id' => $customer_id,
                     'amount' => $item->getData('price'),
                     'action' => $item->getData('sku'),
-                    'action_time' =>$item->getData('created_at')
-                ];
-                $history->addData($newData);
-                $history->save();
+                    'action_time' => $item->getData('created_at')
+                ])->save();
             }
+        }
+
+        foreach ($quote->getAllItems() as $qitem) {
+            $quoteItems[$qitem->getId()] = $qitem;
         }
         $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
         $logger = new \Zend\Log\Logger();
         $logger->addWriter($writer);
-        $logger->debug('TTTTTTTTTTTTTT');
-        $logger->debug('TTTTTTTTTTTTTT');
+        $logger->debug($increment_id);
+        $logger->debug($quote->get());
+        return $this;
     }
 }
